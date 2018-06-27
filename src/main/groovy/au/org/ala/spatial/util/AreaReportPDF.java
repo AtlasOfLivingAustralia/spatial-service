462c236126d8fedd554d608ecfaed4444d88e6db
Qifeng <qifeng.bai@csiro.au>
2018-06-27 16:21:31 +1000

    #110 debuging export pdf on service

--- src/main/groovy/au/org/ala/spatial/util/AreaReportPDF.java
+++ src/main/groovy/au/org/ala/spatial/util/AreaReportPDF.java
@@ -174,6 +174,9 @@ public class AreaReportPDF {
             //read data
 
             JSONParser jp = new JSONParser();
+            String tabulationFile = filePath + "/tabulations.json";
+            LOGGER.debug("Temp tabulation file: " + tabulationFile);
+            LOGGER.debug(FileUtils.readFileToString(new File(filePath + "/tabulations.json")));
             JSONObject tabulations = (JSONObject) jp.parse(FileUtils.readFileToString(new File(filePath + "/tabulations.json"), "UTF-8"));
             JSONObject csvs = (JSONObject) jp.parse(FileUtils.readFileToString(new File(filePath + "/csvs.json")));
             JSONObject counts = (JSONObject) jp.parse(FileUtils.readFileToString(new File(filePath + "/counts.json"), "UTF-8"));
