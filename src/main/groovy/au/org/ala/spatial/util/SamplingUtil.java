a91d6308331d28733903d59b3b0c553db66f7a1b
Qifeng <qifeng.bai@csiro.au>
2021-08-20 11:20:36 +1000

    #183 fix(reverse lat/lng)

--- src/main/groovy/au/org/ala/spatial/util/SamplingUtil.java
+++ src/main/groovy/au/org/ala/spatial/util/SamplingUtil.java
@@ -28,123 +28,124 @@ public class SamplingUtil {
 
     public static List<String> sample(String spatialServiceUrl, String[] layers, double[][] points) {
         ArrayList<String> output = null;
+        long start = System.currentTimeMillis();
 
-
-            long start = System.currentTimeMillis();
-
-            String fields = StringUtils.join(layers, ",");
-            String flatPoints = Arrays.toString(Arrays.stream(points)
-                    .flatMapToDouble(Arrays:: stream)
-                    .toArray());
-            String p = flatPoints.substring(1,flatPoints.length() -1); //remove []
-            String requestBody = "fids=" + fields + "&points=" + p;
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.TEXT_PLAIN);
-            HttpEntity<String> request = new HttpEntity<String>(
-                    requestBody, headers);
-
-            try {
-                RestOperations restTemplate = new RestTemplate();
-                ResponseEntity<String> taskResponse = restTemplate.postForEntity(spatialServiceUrl + "/intersect/batch", request, String.class);
-
-                if (taskResponse.getStatusCode() == HttpStatus.OK) {
-                    JSONObject taskResult = JSONObject.fromObject(taskResponse.getBody());
-                    String checkUrl = taskResult.getString("statusUrl");
-
-                    //check status
-                    boolean notFinished = true;
-                    String downloadUrl = null;
-                    while (notFinished) {
-                        //wait 5s before querying status
-                        Thread.sleep(5000);
-
-                        JSONObject jo = JSONObject.fromObject(Util.getUrl(checkUrl));
-
-                        if (jo.containsKey("error")) {
-                            notFinished = false;
-                        } else if (jo.containsKey("status")) {
-                            String status = jo.getString("status");
-
-                            if ("finished".equals(status)) {
-                                downloadUrl = jo.getString("downloadUrl");
-                                notFinished = false;
-                            } else if ("cancelled".equals(status) || "error".equals(status)) {
-                                notFinished = false;
-                            }
-                        }
-                    }
-
-                    ZipInputStream zis = null;
-                    CSVReader csv = null;
-                    InputStream is = null;
-                    ArrayList<StringBuilder> tmpOutput = new ArrayList<StringBuilder>();
-                    long mid = System.currentTimeMillis();
-                    try {
-                        is = new URI(downloadUrl).toURL().openStream();
-                        zis = new ZipInputStream(is);
-                        ZipEntry ze = zis.getNextEntry();
-                        csv = new CSVReader(new InputStreamReader(zis));
-
-                        for (int i = 0; i < layers.length; i++) {
-                            tmpOutput.add(new StringBuilder());
-                        }
-                        String[] line;
-                        int row = 0;
-                        csv.readNext(); //discard header
-                        while ((line = csv.readNext()) != null) {
-                            //order is consistent with request
-                            for (int i = 2; i < line.length && i - 2 < tmpOutput.size(); i++) {
-                                if (row > 0) {
-                                    tmpOutput.get(i - 2).append("\n");
-                                }
-                                tmpOutput.get(i - 2).append(line[i]);
-                            }
-                            row++;
-                        }
-
-                    } catch (Exception e) {
-                        logger.error(e.getMessage(), e);
-                    } finally {
-                        if (zis != null) {
-                            try {
-                                zis.close();
-                            } catch (Exception e) {
-                                logger.error(e.getMessage(), e);
-                            }
-                        }
-                        if (is != null) {
-                            try {
-                                is.close();
-                            } catch (Exception e) {
-                                logger.error(e.getMessage(), e);
-                            }
-                        }
-                        if (csv != null) {
-                            try {
-                                csv.close();
-                            } catch (Exception e) {
-                                logger.error(e.getMessage(), e);
-                            }
-                        }
-                    }
-
-                    output = new ArrayList<String>();
-                    for (int i = 0; i < tmpOutput.size(); i++) {
-                        output.add(tmpOutput.get(i).toString());
-                        tmpOutput.set(i, null);
-                    }
-
-                    long end = System.currentTimeMillis();
-
-                    logger.info("sample time for " + layers.length + " layers and " + 3 + " coordinates: get response="
-                            + (mid - start) + "ms, write response=" + (end - mid) + "ms");
-
-                } else {
-                    logger.error("Batch intersect failed: " + taskResponse.getStatusCode());
-                }
-            } catch(Exception e) {
-                logger.error("Failed to create a layer intersect task: " + e.getMessage());
+        String fields = StringUtils.join(layers, ",");
+        String strPoints = "";
+        for (int i = 0; i < points.length; i++) {
+            if (i > 0) {
+                strPoints += ",";
             }
+            strPoints += (String.valueOf(points[i][1])) + "," + String.valueOf(points[i][0]);
+        }
+        String requestBody = "fids=" + fields + "&points=" + strPoints;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        HttpEntity<String> request = new HttpEntity<String>(
+                requestBody, headers);
+
+        try {
+            RestOperations restTemplate = new RestTemplate();
+            ResponseEntity<String> taskResponse = restTemplate.postForEntity(spatialServiceUrl + "/intersect/batch", request, String.class);
+
+            if (taskResponse.getStatusCode() == HttpStatus.OK) {
+                JSONObject taskResult = JSONObject.fromObject(taskResponse.getBody());
+                String checkUrl = taskResult.getString("statusUrl");
+
+                //check status
+                boolean notFinished = true;
+                String downloadUrl = null;
+                while (notFinished) {
+                    //wait 5s before querying status
+                    Thread.sleep(5000);
+
+                    JSONObject jo = JSONObject.fromObject(Util.getUrl(checkUrl));
+
+                    if (jo.containsKey("error")) {
+                        notFinished = false;
+                    } else if (jo.containsKey("status")) {
+                        String status = jo.getString("status");
+
+                        if ("finished".equals(status)) {
+                            downloadUrl = jo.getString("downloadUrl");
+                            notFinished = false;
+                        } else if ("cancelled".equals(status) || "error".equals(status)) {
+                            notFinished = false;
+                        }
+                    }
+                }
+
+                ZipInputStream zis = null;
+                CSVReader csv = null;
+                InputStream is = null;
+                ArrayList<StringBuilder> tmpOutput = new ArrayList<StringBuilder>();
+                long mid = System.currentTimeMillis();
+                try {
+                    is = new URI(downloadUrl).toURL().openStream();
+                    zis = new ZipInputStream(is);
+                    ZipEntry ze = zis.getNextEntry();
+                    csv = new CSVReader(new InputStreamReader(zis));
+
+                    for (int i = 0; i < layers.length; i++) {
+                        tmpOutput.add(new StringBuilder());
+                    }
+                    String[] line;
+                    int row = 0;
+                    csv.readNext(); //discard header
+                    while ((line = csv.readNext()) != null) {
+                        //order is consistent with request
+                        for (int i = 2; i < line.length && i - 2 < tmpOutput.size(); i++) {
+                            if (row > 0) {
+                                tmpOutput.get(i - 2).append("\n");
+                            }
+                            tmpOutput.get(i - 2).append(line[i]);
+                        }
+                        row++;
+                    }
+
+                } catch (Exception e) {
+                    logger.error(e.getMessage(), e);
+                } finally {
+                    if (zis != null) {
+                        try {
+                            zis.close();
+                        } catch (Exception e) {
+                            logger.error(e.getMessage(), e);
+                        }
+                    }
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (Exception e) {
+                            logger.error(e.getMessage(), e);
+                        }
+                    }
+                    if (csv != null) {
+                        try {
+                            csv.close();
+                        } catch (Exception e) {
+                            logger.error(e.getMessage(), e);
+                        }
+                    }
+                }
+
+                output = new ArrayList<String>();
+                for (int i = 0; i < tmpOutput.size(); i++) {
+                    output.add(tmpOutput.get(i).toString());
+                    tmpOutput.set(i, null);
+                }
+
+                long end = System.currentTimeMillis();
+
+                logger.info("sample time for " + layers.length + " layers and " + 3 + " coordinates: get response="
+                        + (mid - start) + "ms, write response=" + (end - mid) + "ms");
+
+            } else {
+                logger.error("Batch intersect failed: " + taskResponse.getStatusCode());
+            }
+        } catch(Exception e) {
+            logger.error("Failed to create a layer intersect task: " + e.getMessage());
+        }
 
         return output;
     }
