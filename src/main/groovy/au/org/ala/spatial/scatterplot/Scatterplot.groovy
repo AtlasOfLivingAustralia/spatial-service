6e5cfaec4e161677af918046fc309edcc22b024a
Adam Collins <adam.collins@csiro.au>
2024-05-27 09:05:38 +1000

    restarting tasks and scatterplot with grid domain

--- src/main/groovy/au/org/ala/spatial/scatterplot/Scatterplot.groovy
+++ src/main/groovy/au/org/ala/spatial/scatterplot/Scatterplot.groovy
@@ -454,6 +454,11 @@ class Scatterplot {
     }
 
     void createBlockPlot(float min1, float max1, float min2, float max2) {
+        if (scatterplotDataDTO.getGridCutoffs() != null) {
+            // already built, do not build again
+            return
+        }
+
         int divisions = scatterplotDTO.getGridDivisions()
 
         //get linear cutoffs
@@ -483,7 +488,7 @@ class Scatterplot {
         String[] layers = scatterplotDTO.getLayers()
 
         // TODO: can we do this without cutting grids?
-        
+
         if (cutDataPath == null) {  //only need to cut once if rebuilding
             String[] types = new String[layers.length]
             for (int i = 0; i < layers.length; i++) {
