05ae0d8570b8b68af62cb8d3862d5d790dd16b4d
Stefan Van Dyck <vandyck.stefan@gmail.com>
2024-10-24 13:20:38 +0200

    inbo/vlaams-biodiversiteitsportaal#116: Always use english locale to prevent issues with number formatting in some others.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -5,6 +5,7 @@ import au.org.ala.spatial.web.NoSSOStrategy
 import com.github.ziplet.filter.compression.CompressingFilter
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.core.Ordered
+import org.springframework.web.servlet.i18n.FixedLocaleResolver
 
 beans = {
     compressionFilter(FilterRegistrationBean) {
@@ -17,4 +18,8 @@ beans = {
     if (!application.config.security.cas.enabled && !application.config.security.oidc.enabled) {
         noSSOStrategy(NoSSOStrategy) {}
     }
+
+    // Use fixed English locale, prevents issues with parsing of BBox decimal values in some locales
+    // https://github.com/AtlasOfLivingAustralia/spatial-service/issues/247
+    localeResolver(FixedLocaleResolver, new Locale('en'))
 }
